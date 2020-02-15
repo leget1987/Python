536f210bbd29fa1226b69ccfdb11b296476efa50
Dmitriy Lobanov <leget@list.ru>
2020-02-15 13:44:59 +0300

    Доработал 1 задание с пустым вводом премии

--- lesson-4/lesson_4_task_1.py
+++ lesson-4/lesson_4_task_1.py
@@ -3,17 +3,17 @@
 # Для выполнения расчета для конкретных значений необходимо запускать скрипт с параметрами.
 from sys import argv
 
-
 if len(argv) == 4:
     print(f'Ваша зарплата - {float(argv[1]) * float(argv[2]) + float(argv[3])}')
 elif len(argv) == 3:
     print(f'Ваша зарплата - {float(argv[1]) * float(argv[2])}')
 else:
-    print("Вы ввели слишком мало параметров, введите количество отработанных часов, "
-          "затем стоимость часа работы и вашу премию")
-    earned_hours = float(input('Сколько часов вы отработали?'))
-    rate_peer_hour = float(input('Какая у вас ставка в час?'))
-    prize = float(input('Какая у вас премия?'))
-    print(f'Вы заработали - {earned_hours * rate_peer_hour + prize}')
-
-
+        print("Вы ввели слишком мало параметров, введите количество отработанных часов, "
+              "затем стоимость часа работы и вашу премию")
+        earned_hours = float(input('Сколько часов вы отработали? '))
+        rate_peer_hour = float(input('Какая у вас ставка в час? '))
+        try:
+            prize = float(input('Какая у вас премия? '))
+            print(f'Вы заработали - {earned_hours * rate_peer_hour + prize}')
+        except ValueError:
+            print(f'Вы заработали - {earned_hours * rate_peer_hour}')
